72b01aa71f3793283b7dbec61e637c7c15058202
PARTH-kodes <officialparth18@gmail.com>
2024-06-20 00:06:24 +0530

    Corrected a logical error

--- DYNAMIC PROGRAMMING/UNBOUNDED KNAPSACK/UnboundedKnapsack.java
+++ DYNAMIC PROGRAMMING/UNBOUNDED KNAPSACK/UnboundedKnapsack.java
@@ -1,26 +1,24 @@
 
 public class UnboundedKnapsack {
-    public static int unboundMemo(int price[], int wt[], int W, int indx, int dp[][]) { // USING MEMOIZATION METHOD
-        if (indx == price.length || W == 0) {
+    public static int unboundMemo(int pr[], int wt[], int W, int indx, int dp[][]) { // USING MEMOIZATION METHOD
+        if (indx == pr.length || W == 0) {
             return 0;
         }
-        if (dp[indx][W] != -1) {
+        if (dp[indx][W] != 0) {
             return dp[indx][W];
         }
         if (wt[indx] <= W) {
             return dp[indx][W] = Math.max(
-                    Math.max(price[indx] + unboundMemo(price, wt, W - wt[indx], indx + 1, dp),
-                            price[indx] + unboundMemo(price, wt, W - wt[indx], indx, dp)),
-                    unboundMemo(price, wt, W, indx + 1, dp));
+                    Math.max(pr[indx] + unboundMemo(pr, wt, W - wt[indx], indx + 1, dp),
+                            pr[indx] + unboundMemo(pr, wt, W - wt[indx], indx, dp)),
+                    unboundMemo(pr, wt, W, indx + 1, dp));
         } else {
-            return dp[indx][W] = unboundMemo(price, wt, W, indx + 1, dp);
+            return dp[indx][W] = unboundMemo(pr, wt, W, indx + 1, dp);
         }
     }
 
-
-
-    public static int unboundTab(int price[], int wt[], int W) { // USING TABULATION METHOD
-        int n = price.length;
+    public static int unboundTab(int pr[], int wt[], int W) { // USING TABULATION METHOD
+        int n = pr.length;
         int dp[][] = new int[n + 1][W + 1];
         // initialise 0th row and 0th column with 0 -> JAVA already fills an integer
         // array with 0s, so no need.
@@ -36,18 +34,18 @@ public class UnboundedKnapsack {
                                               */
             for (int j = 1; j < dp[0].length; j++) {
                 if (wt[i - 1] <= j) {
-                    dp[i][j] = Math.max(price[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);/*
-                                                                                            * therefore evne after
-                                                                                            * selecting the ith element
-                                                                                            * from the price array, we
-                                                                                            * can still select it again
-                                                                                            * among the remaining
-                                                                                            * elements, so unlike
-                                                                                            * 0-1knapsack where we i-1
-                                                                                            * choices after selecting 1
-                                                                                            * element, here we have i
-                                                                                            * choices again
-                                                                                            */
+                    dp[i][j] = Math.max(pr[i - 1] + dp[i][j - wt[i - 1]], dp[i - 1][j]);/*
+                                                                                         * therefore evne after
+                                                                                         * selecting the ith element
+                                                                                         * from the price array, we
+                                                                                         * can still select it again
+                                                                                         * among the remaining
+                                                                                         * elements, so unlike
+                                                                                         * 0-1knapsack where we i-1
+                                                                                         * choices after selecting 1
+                                                                                         * element, here we have i
+                                                                                         * choices again
+                                                                                         */
                 } else { /* P.S. compare the if clause of both the programs. */
                     dp[i][j] = dp[i - 1][j];
                 }
@@ -57,11 +55,11 @@ public class UnboundedKnapsack {
     }
 
     public static void main(String[] args) {
-        int price[] = { 15, 14, 10, 45, 30 };
+        int pr[] = { 15, 14, 10, 45, 30 };
         int wt[] = { 2, 5, 1, 3, 4 };
         int W = 7;
 
-        System.out.println(unboundTab(price, wt, W));
-        System.out.println(unboundMemo(price, wt, W, 0, new int[price.length + 1][W + 1]));
+        System.out.println(unboundTab(pr, wt, W));
+        System.out.println(unboundMemo(pr, wt, W, 0, new int[pr.length + 1][W + 1]));
     }
 }
