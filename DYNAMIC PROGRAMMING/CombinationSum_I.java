0f6737c097c056844b458d1841fe501192e2a93a
PARTH-kodes <officialparth18@gmail.com>
2024-07-13 18:11:09 +0530

    Enhanced

--- DYNAMIC PROGRAMMING/CombinationSum_I.java
+++ DYNAMIC PROGRAMMING/CombinationSum_I.java
@@ -3,11 +3,12 @@ import java.util.*;
 /* this code finds all the possible combinations of the elements of the arr, which result in the sum = target. Repeating a same element is allowed.*/
 
 public class CombinationSum_I {
-    public static void findCombination(int indx, int arr[], int target, List<List<Integer>> ans,
-            List<Integer> current) {
+    public static void findCombination(int indx, int arr[], int target, List<List<Integer>> ans, List<Integer> current) {
 
         if (indx == arr.length) {
+
             if (target == 0) {
+
                 ans.add(new ArrayList<>(current)); /*
                                                     * ans.add(new ArrayList<>(current)); is adding a copy of the current
                                                     * combination (stored in current) to the list of combinations (ans).
@@ -15,23 +16,58 @@ public class CombinationSum_I {
                                                     * changes made to current afterwards.
                                                     */
             }
+
             return;
+
         }
+
         if (arr[indx] <= target) {
+
             current.add(arr[indx]);
+
             findCombination(indx, arr, target - arr[indx], ans, current);
-            current.removeLast();
+
+            current.remove(current.size() - 1);
+
         }
+
         findCombination(indx + 1, arr, target, ans, current);
+
     }
 
     public static void main(String[] args) {
-        int arr[] = { 2, 3, 6, 7 };
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+
+        int size = sc.nextInt();
+        
+        int arr[] = new int[size];
+
+        System.out.println("Enter the elements of the array: ");
+
+        for (int i = 0; i < size; i++) {
+
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter the target: ");
+
+        int target = sc.nextInt();
+
         List<Integer> ds = new ArrayList<>();
+
         List<List<Integer>> result = new ArrayList<>();
-        int target = 7;
+
+        Arrays.sort(arr);
+        
         findCombination(0, arr, target, result, ds);
 
         System.out.println(result);
+
+        sc.close();
+
     }
+
 }
